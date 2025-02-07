79925b0be1563a3071a0196f114ee8507f4f8775
Dhanu-Dc <dhanu323246@gmail.com>
2025-02-07 11:11:59 +0530

    Add files via upload

---
+++ f.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbUlponXpYQXigTszOERmgRCIcwcLoRDE",
+  authDomain: "gamers-souls.firebaseapp.com",
+  projectId: "gamers-souls",
+  storageBucket: "gamers-souls.firebasestorage.app",
+  messagingSenderId: "339442699256",
+  appId: "1:339442699256:web:894205014c4af8b5d4e40d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
